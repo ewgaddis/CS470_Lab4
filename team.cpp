cb2d58ecbcd1c002abc8e19f697f418ef71613e2
Eli <eli.gaddis@gmail.com>
2015-12-04 13:58:42 -0700

    Fixed team.cpp

--- team.cpp
+++ team.cpp
@@ -13,11 +13,8 @@
 #include "lineAgent.h"
 #include "crazyAgent.h"
 #include "geometry.h"
-<<<<<<< HEAD
 #include "kalmanAgent.h"
-=======
 #include "kalmanFilter.h"
->>>>>>> a998afc4c0b397c3bdf650df7f8d41ada5ed212e
 
 #include <Eigen/Dense>
 #include <iostream>
@@ -86,7 +83,6 @@ bool robot_update()
 					 grid->getGrid(),
 					 &obstacles);
 	*/
-<<<<<<< HEAD
 	Vector v(2.4, -1.9);
 
 	Vector2d v2;
@@ -95,17 +91,13 @@ bool robot_update()
 	//cout << v2 << endl;
 
 	v.setEVector(v2);
-=======
->>>>>>> a998afc4c0b397c3bdf650df7f8d41ada5ed212e
 
 	/*scout1->Update(obstacles, grid);
 	scout2->Update(obstacles, grid);
 	scout3->Update(obstacles, grid);
 	scout4->Update(obstacles, grid);
 	scout5->Update(obstacles, grid);*/
-<<<<<<< HEAD
 	//cout << v << endl;
-=======
 
 	KalmanFilter filter(100.0, 0.1, 0.1,
 						0.1, 0.1, 100.0,
@@ -141,7 +133,6 @@ bool robot_update()
 	plotter.drawCircle(pos, 10, 255, 0, 0);
 	plotter.drawGaussian(filter.getMean(), filter.getSigma());
 	plotter.finishFile();
->>>>>>> a998afc4c0b397c3bdf650df7f8d41ada5ed212e
 
 	//return false;
 	return true;
